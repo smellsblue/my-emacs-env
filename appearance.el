d39052e2a57c30f0731a6601ffc0a577dae4276c
Mike Virata-Stone <mike@virata-stone.com>
2013-11-15 23:44:04 -0800

    Include color theme library, and install it as part of appearance.

--- appearance.el
+++ appearance.el
@@ -1,4 +1,8 @@
 (when (display-graphic-p)
-  (when (locate-library "color-theme")
-    (load (concat (file-name-directory load-file-name) "color-theme-molokai/color-theme-molokai.el"))
-    (color-theme-molokai)))
+  (add-to-list 'load-path (concat (file-name-directory load-file-name) "color-theme-6.6.0"))
+  (require 'color-theme)
+  (eval-after-load "color-theme"
+    '(progn
+       (color-theme-initialize)
+       (load (concat (file-name-directory load-file-name) "color-theme-molokai/color-theme-molokai.el"))
+       (color-theme-molokai))))
