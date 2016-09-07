f9c9e6e095fad0446a70f2fd070992747e43f992
Mike Virata-Stone <mjstone@on-site.com>
2016-09-07 10:41:41 -0700

    Fix bug that makes it hard to close emacs sometimes

--- shell-size.el
+++ shell-size.el
@@ -1,13 +1,12 @@
-;; From http://stackoverflow.com/a/11255996/122
+;; From http://stackoverflow.com/a/20015336/122
 (defun comint-fix-window-size ()
   "Change process window size."
   (when (derived-mode-p 'comint-mode)
-    (set-process-window-size (get-buffer-process (current-buffer))
-                             (window-height)
-                             (window-width))))
+    (let ((process (get-buffer-process (current-buffer))))
+      (unless (eq nil process)
+        (set-process-window-size process (window-height) (window-width))))))
 
-(defun my-shell-mode-hook ()
-  ;; add this hook as buffer local, so it runs once per window.
-  (add-hook 'window-configuration-change-hook 'comint-fix-window-size nil t))
-
-(add-hook 'shell-mode-hook 'my-shell-mode-hook)
+(add-hook 'shell-mode-hook
+          (lambda ()
+            ;; add this hook as buffer local, so it runs once per window.
+            (add-hook 'window-configuration-change-hook 'comint-fix-window-size nil t)))
