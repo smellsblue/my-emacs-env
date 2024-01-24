b6bc09204ff419332620d27ebcae109c7c6b6875
Mike Virata-Stone <stonemj@gmail.com>
2024-01-23 20:53:53 -0800

    Fix default window size and make it configurable

--- mac-init.el
+++ mac-init.el
@@ -10,14 +10,15 @@
   "Whether or not to initialize window size on a mac"
   :type 'boolean
   :group 'mvs)
+(defcustom mvs-mac-window-width 159
+  "When initializing window size on a mac, use this width"
+  :type 'integer
+  :group 'mvs)
 (setq mvs-font mvs-mac-font)
 
 ;;; Nice size for the default window
 (if mvs-mac-initialize-window-size
     (progn
-      (defun get-default-height ()
-        (/ (- (display-pixel-height) 120)
-           (frame-char-height)))
-
-      (add-to-list 'default-frame-alist '(width . 159))
-      (add-to-list 'default-frame-alist (cons 'height (get-default-height)))))
+      (add-to-list 'default-frame-alist '(left . 0))
+      (add-to-list 'default-frame-alist (cons 'width mvs-mac-window-width))
+      (add-to-list 'default-frame-alist '(fullscreen . fullheight))))
